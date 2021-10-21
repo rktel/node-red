f9226e7917c92e02032ce0c9f79855445207a27e
rktel <rakinakuy@hotmail.com>
2021-10-21 15:54:00 -0500

    onsenui 2

---
+++ imports/ui/App.svelte
@@ -0,0 +1,7 @@
+<label class="switch switch--material">
+    <input type="checkbox" class="switch__input switch--material__input">
+    <div class="switch__toggle switch--material__toggle">
+      <div class="switch__handle switch--material__handle">
+      </div>
+    </div>
+  </label>
