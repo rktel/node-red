409c2cf9155fa0f670cc4865f28e28bdacb54b28
rktel <rakinakuy@hotmail.com>
2021-10-21 13:21:35 -0500

    f7 remove f7-svelte

--- client/main.js
+++ client/main.js
@@ -1,9 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import Framework7 from 'framework7/lite-bundle';
-import Framework7Svelte from 'framework7-svelte';
-import App from '../imports/ui/App.svelte';
-import 'framework7/framework7-bundle.css';
-Framework7.use(Framework7Svelte);
 
 
   new App({
